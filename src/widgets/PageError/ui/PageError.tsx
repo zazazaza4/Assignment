92e5bba896d5a5afd2b5bcc27d35b3e793c48fc2
Yaroslav <6715414@gmail.com>
2024-03-03 17:14:15 +0200

    feat: add PageError widget

---
+++ src/widgets/PageError/ui/PageError.tsx
@@ -0,0 +1,27 @@
+import { FC } from 'react';
+
+import { classNames } from '@/shared/lib/classNames/classNames';
+import { Button } from '@/shared/ui/Button';
+import { VStack } from '@/shared/ui/Stack';
+import { Text } from '@/shared/ui/Text';
+
+import cls from './PageError.module.scss';
+
+interface PageErrorProps {
+  className?: string;
+}
+
+export const PageError: FC<PageErrorProps> = ({ className }) => {
+  const reloadPage = () => {
+    window.location.reload();
+  };
+
+  return (
+    <VStack gap="8" className={classNames(cls.PageError, {}, [className])}>
+      <Text theme="black" title="Some went wrong" />
+      <Button className={cls.button} onClick={reloadPage}>
+        Reload page
+      </Button>
+    </VStack>
+  );
+};
